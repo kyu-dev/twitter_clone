fe73ec418a16b0603e7ff642857b7625dd1b7582
Arthur Vienot Ongaro <arthurvienot1008@gmail.com>
2025-07-13 15:02:21 +0200

    add login page and session handling

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,37 @@
+'use client';
+import { useSession } from 'next-auth/react';
+import { signOut } from 'next-auth/react';
+import Image from 'next/image';
+import Link from 'next/link';
+
 export default function () {
+  const { data: session } = useSession();
   return (
     <>
-      <h1>Hello</h1>
-      <button className="btn">click</button>
+      {session?.user ? (
+        <>
+          {session?.user?.image && (
+            <Image
+              src={session.user.image}
+              alt="user avatar"
+              width={32}
+              height={32}
+            />
+          )}
+          {session.user.name && <span>{session.user.name}</span>}
+          <button
+            onClick={() => {
+              signOut();
+            }}
+          >
+            Déconexion
+          </button>
+        </>
+      ) : (
+        <Link href="/login">
+          <button>conexion</button>
+        </Link>
+      )}
     </>
   );
 }
